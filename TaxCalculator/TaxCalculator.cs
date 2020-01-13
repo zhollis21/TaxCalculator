1de77ae7bbfb88e438f6a3e10de141932ba6aa1d
Zach Hollis <zhollis21@gmail.com>
2020-01-13 17:50:47 -0500

    Rearranged the functions and changed 'prompt' to 'question'

--- TaxCalculator/TaxCalculator.cs
+++ TaxCalculator/TaxCalculator.cs
@@ -47,105 +47,6 @@ namespace TaxCalculator
             IdentifyExemptIncome();
         }
 
-        private void IdentifyExemptIncome()
-        {
-            _taxExemptIncome = 0;
-
-            bool has401k = GetYesOrNoAnswer("Do you have a non-Roth 401k?");
-            if (has401k)
-            {
-                var percentContribution = GetNonNegativePercent("Enter the contribution percentage");
-                var nontaxable401kIncome = percentContribution * _grossIncome;
-
-                Console.WriteLine(
-                    $"{percentContribution.ToString("P")} of {_grossIncome.ToString("C")} contributed, " +
-                    $"for a total of {nontaxable401kIncome.ToString("C")} to your 401k.");
-
-                _taxExemptIncome += nontaxable401kIncome;
-            }
-
-            if (_isMarried)
-            {
-                has401k = GetYesOrNoAnswer("Does your spouse have a non-Roth 401k?");
-                if (has401k)
-                {
-                    var spousePercentContribution = GetNonNegativePercent("Enter the contribution percentage");
-                    var spouseNontaxable401kIncome = spousePercentContribution * _spouseGrossIncome;
-
-                    Console.WriteLine(
-                        $"{spousePercentContribution.ToString("P")} of {_grossIncome.ToString("C")} contributed, " +
-                        $"for a total of {spouseNontaxable401kIncome.ToString("C")} to your spouse's 401k.");
-
-                    _taxExemptIncome += spouseNontaxable401kIncome;
-                }
-            }
-
-            Console.WriteLine();
-        }
-
-        private static bool GetYesOrNoAnswer(string prompt)
-        {
-            Console.Write($"\n{prompt} ('y' or 'n'): ");
-
-            char answer = Console.ReadLine().ToLower().FirstOrDefault();
-
-            while (answer != 'y' && answer != 'n')
-            {
-                Console.Write("Invalid Answer... Please respond with 'y' or 'n': ");
-                answer = Console.ReadLine().ToLower().FirstOrDefault();
-            }
-
-            return answer == 'y';
-        }
-
-        private static long GetNonNegativeLong(string prompt)
-        {
-            Console.Write($"\n{prompt}: ");
-            var isValidLong = long.TryParse(Console.ReadLine(), out long answer);
-
-            while (!isValidLong || answer < 0)
-            {
-                Console.Write("Invalid Answer... Please respond with a valid/non-negative number: ");
-                isValidLong = long.TryParse(Console.ReadLine(), out answer);
-            }
-
-            return answer;
-        }
-
-        private static double GetNonNegativePercent(string prompt)
-        {
-            Console.Write($"\n{prompt} (0 - 100): ");
-            var isValidDouble = double.TryParse(Console.ReadLine(), out double answer);
-
-            while (!isValidDouble || answer < 0 || answer > 100)
-            {
-                Console.Write("Invalid Answer... Please respond with a valid/non-negative number between 0 and 100: ");
-                isValidDouble = double.TryParse(Console.ReadLine(), out answer);
-            }
-
-            return answer / 100;
-        }
-
-        private void GetMarriageInformation()
-        {
-            _isMarried = GetYesOrNoAnswer("Are you legally married?");
-            Console.WriteLine();
-        }
-
-        private void GetIncomeInformation()
-        {
-            _grossIncome = 0;
-
-            _grossIncome += GetNonNegativeLong("Enter your yearly income");
-
-            if (_isMarried)
-            {
-                _spouseGrossIncome += GetNonNegativeLong("Enter your spouse's yearly income");
-            }
-
-            Console.WriteLine();
-        }
-
         public void CalculateTaxEstimation()
         {
             double untaxedIncome = (_grossIncome + _spouseGrossIncome) - _taxExemptIncome;
@@ -189,6 +90,105 @@ namespace TaxCalculator
                 $"\nTotal Net Income: {((totalIncome - taxEstimate) - _taxExemptIncome).ToString("C")}" +
                 $"\nTotal Taxes: {taxEstimate.ToString("C")}" +
                 $"\nAverage Percent Taxed: {(taxEstimate / (totalIncome - _taxExemptIncome)).ToString("P")}");
+        }
+
+        private void IdentifyExemptIncome()
+        {
+            _taxExemptIncome = 0;
+
+            bool has401k = GetYesOrNoAnswer("Do you have a non-Roth 401k?");
+            if (has401k)
+            {
+                var percentContribution = GetNonNegativePercent("Enter the contribution percentage");
+                var nontaxable401kIncome = percentContribution * _grossIncome;
+
+                Console.WriteLine(
+                    $"{percentContribution.ToString("P")} of {_grossIncome.ToString("C")} contributed, " +
+                    $"for a total of {nontaxable401kIncome.ToString("C")} to your 401k.");
+
+                _taxExemptIncome += nontaxable401kIncome;
+            }
+
+            if (_isMarried)
+            {
+                has401k = GetYesOrNoAnswer("Does your spouse have a non-Roth 401k?");
+                if (has401k)
+                {
+                    var spousePercentContribution = GetNonNegativePercent("Enter the contribution percentage");
+                    var spouseNontaxable401kIncome = spousePercentContribution * _spouseGrossIncome;
+
+                    Console.WriteLine(
+                        $"{spousePercentContribution.ToString("P")} of {_grossIncome.ToString("C")} contributed, " +
+                        $"for a total of {spouseNontaxable401kIncome.ToString("C")} to your spouse's 401k.");
+
+                    _taxExemptIncome += spouseNontaxable401kIncome;
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        private void GetMarriageInformation()
+        {
+            _isMarried = GetYesOrNoAnswer("Are you legally married?");
+            Console.WriteLine();
+        }
+
+        private void GetIncomeInformation()
+        {
+            _grossIncome = 0;
+
+            _grossIncome += GetNonNegativeLong("Enter your yearly income");
+
+            if (_isMarried)
+            {
+                _spouseGrossIncome += GetNonNegativeLong("Enter your spouse's yearly income");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static bool GetYesOrNoAnswer(string question)
+        {
+            Console.Write($"\n{question} ('y' or 'n'): ");
+
+            char answer = Console.ReadLine().ToLower().FirstOrDefault();
+
+            while (answer != 'y' && answer != 'n')
+            {
+                Console.Write("Invalid Answer... Please respond with 'y' or 'n': ");
+                answer = Console.ReadLine().ToLower().FirstOrDefault();
+            }
+
+            return answer == 'y';
+        }
+
+        private static long GetNonNegativeLong(string question)
+        {
+            Console.Write($"\n{question}: ");
+            var isValidLong = long.TryParse(Console.ReadLine(), out long answer);
+
+            while (!isValidLong || answer < 0)
+            {
+                Console.Write("Invalid Answer... Please respond with a valid/non-negative number: ");
+                isValidLong = long.TryParse(Console.ReadLine(), out answer);
+            }
+
+            return answer;
+        }
+
+        private static double GetNonNegativePercent(string question)
+        {
+            Console.Write($"\n{question} (0 - 100): ");
+            var isValidDouble = double.TryParse(Console.ReadLine(), out double answer);
+
+            while (!isValidDouble || answer < 0 || answer > 100)
+            {
+                Console.Write("Invalid Answer... Please respond with a valid/non-negative number between 0 and 100: ");
+                isValidDouble = double.TryParse(Console.ReadLine(), out answer);
+            }
+
+            return answer / 100;
         }
     }
 }
